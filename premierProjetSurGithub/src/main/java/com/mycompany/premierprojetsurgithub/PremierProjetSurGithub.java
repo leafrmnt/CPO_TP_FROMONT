06f0d8aa8c0f162992aa5bec2a950baf6e74431f
lea <lea.fromont@epfedu.fr>
2023-09-15 10:34:15 +0200

    Création d'un nouveau projet versionné

---
+++ premierProjetSurGithub/src/main/java/com/mycompany/premierprojetsurgithub/PremierProjetSurGithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.premierprojetsurgithub;
+
+/**
+ *
+ * @author leafr
+ */
+public class PremierProjetSurGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
