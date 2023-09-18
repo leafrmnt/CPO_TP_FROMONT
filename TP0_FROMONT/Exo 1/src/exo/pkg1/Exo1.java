9547001a0f805c37059394dfcb5ac2506bea17af
lea <lea.fromont@epfedu.fr>
2023-09-18 12:56:45 +0200

    Exo 1 TD0 fini

---
+++ TP0_FROMONT/Exo 1/src/exo/pkg1/Exo1.java
@@ -0,0 +1,31 @@
+/*Fromont Léa
+ * TDB
+ *Exo 1 TD0 
+ *18/09/2023
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author leafr
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("Aurevoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Salut, tu t'apelles comment ?");
+        prenom = sc.nextLine();
+    }
+    
+}
