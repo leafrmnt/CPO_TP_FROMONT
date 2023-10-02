aea4eb64e3db1b91069221628f2644e39c0e17e3
lea <lea.fromont@epfedu.fr>
2023-10-02 21:20:39 +0200

    TP1 guessMyNumber fini

--- TP0_FROMONT/TP1_guessMyNumber_FROMONT/src/tp1_guessmynumber_fromont/TP1_guessMyNumber_FROMONT.java
+++ TP0_FROMONT/TP1_guessMyNumber_FROMONT/src/tp1_guessmynumber_fromont/TP1_guessMyNumber_FROMONT.java
@@ -7,6 +7,7 @@
  */
 package tp1_guessmynumber_fromont;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -27,9 +28,99 @@ public class TP1_guessMyNumber_FROMONT {
             System.out.println("Nombre aléatoire :" + nombreAleatoire);
             i++;
         }
-    }
-}
+
         
+        int n = generateurAleat.nextInt(100);
+        int devine;
+        int tentative = 0;
+
+        Scanner sc = new Scanner(System.in);
+        int choix;
+        int difference;
+        Scanner nu = new Scanner(System.in);
+        System.out.println("""
+                           
+                            Saisissez la conversion que vous souhaiter effectuer en ecrivant le numero correspondant:
+                           1) Niveau facile
+                           2) Niveau intermediaire
+                           3) Niveau dificile """);
+        choix = nu.nextInt();
+        System.out.println("\n Devinez le nombre entre 0 et 99 !");
+        
+        
+        switch (choix) {
+            case 1 -> {
+                while (true) { // Pas de limite de tentatives pour le niveau facile
+                    System.out.print("Entrez votre proposition : ");
+                    devine = sc.nextInt();
+                    difference = n - devine;
+                    
+                    
+                    if (difference > 20) {
+                        System.out.println("\n Vraiment trop " + (devine > n ? "grand" : "petit"));
+                    } else if (devine > n) {
+                        System.out.println("\n Trop grand");
+                    } else if (devine < n) {
+                        System.out.println("\n Trop petit");
+                    } else {
+                        System.out.println("\n Gagné !");
+                        System.out.println("\n Vous avez eu " + (tentative + 1) + " tentative(s)");
+                        break;
+                    }
+                }
+            }
+            case 2 -> {
+                // Niveau intermédiaire (par défaut)
+                while (true) {
+                    System.out.print("Entrez votre proposition : ");
+                    devine = sc.nextInt();
+                    
+                    if (devine > n) {
+                        tentative = tentative + 1;
+                        System.out.println("\n Trop grand");
+                    } else if (devine < n) {
+                        tentative = tentative + 1;
+                        System.out.println("\n Trop petit");
+                        
+                    } else {
+                        tentative = tentative + 1;
+                        System.out.println("\n Gagne !");
+                        System.out.println("\n Vous avez eu " + tentative + "tentatives");
+                        break;
+                    }
+                }
+            }
+            case 3 -> {
+                // Niveau difficile (3 tentatives)
+                for (int i = 0; i < 3; i++) {
+                    System.out.print("Entrez votre proposition : ");
+                    devine = sc.nextInt();
+                    
+                    if (devine > n) {
+                        System.out.println("\n Trop grand");
+                    } else if (devine < n) {
+                        System.out.println("\n Trop petit");
+                    } else {
+                        System.out.println("\n Gagné !");
+                        tentative= tentative +1;
+                        System.out.println("\n Vous avez eu " + tentative  + " tentatives");
+                        
+                    }
+                    if (tentative >= 3) {
+                    System.out.println("\n Vous avez épuisé toutes vos tentatives. Le nombre était : " + n);
+                    break;
+                    }
+                }
+            }
+            default -> System.out.println("Niveau non valide. Choisissez parmi les niveaux 1, 2 ou 3.");
+        }
+            
+            }
+    }
+    
+
+
+
     
     
 
