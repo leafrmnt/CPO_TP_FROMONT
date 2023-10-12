a5afd5281e501abe39f9a559c36d28985b48fd3e
lea <lea.fromont@epfedu.fr>
2023-10-12 19:16:42 +0200

    TP2 exo1 en cours

--- TP0_FROMONT/TP2_Bieres_FROMONT/src/tp2_bieres_fromont/TP2_Bieres_FROMONT.java
+++ TP0_FROMONT/TP2_Bieres_FROMONT/src/tp2_bieres_fromont/TP2_Bieres_FROMONT.java
@@ -17,18 +17,12 @@ public class TP2_Bieres_FROMONT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere() ;
-        uneBiere.Nom = "Cuvée des trolls";
-        uneBiere.degreAlcool = 7.0 ;
-        uneBiere.brasserie = "Dubuisson";
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ;
         uneBiere.ouverte = false;
         uneBiere.lireEtiquette();
-        BouteilleBiere deuxiemeBiere = new BouteilleBiere() ;
-        deuxiemeBiere.Nom = "Leffe";
-        deuxiemeBiere.degreAlcool = 6.6 ;
-        deuxiemeBiere.brasserie = "Abbaye de Leffe";
-        deuxiemeBiere.ouverte = false;
-        deuxiemeBiere.lireEtiquette();
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
+        autreBiere.lireEtiquette();
+        
 
     }
     
