6d830b4c07d04b63e838e64068024cf5306e3654
lea <lea.fromont@epfedu.fr>
2023-10-13 10:38:24 +0200

    TP2 exo1 fini

--- TP0_FROMONT/TP2_Bieres_FROMONT/src/tp2_bieres_fromont/TP2_Bieres_FROMONT.java
+++ TP0_FROMONT/TP2_Bieres_FROMONT/src/tp2_bieres_fromont/TP2_Bieres_FROMONT.java
@@ -19,10 +19,52 @@ public class TP2_Bieres_FROMONT {
     public static void main(String[] args) {
         BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ;
         uneBiere.ouverte = false;
-        uneBiere.lireEtiquette();
-        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
-        autreBiere.lireEtiquette();
+        if (uneBiere.Decapsuler()) {
+            System.out.println("biere ouverte");
+            uneBiere.ouverte= true;
+        } else {
+            System.out.println("erreur : biere déjà ouverte");
+        }
+        System.out.println(uneBiere) ;
         
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
+        autreBiere.ouverte = false;
+         if (autreBiere.Decapsuler()) {
+            System.out.println("biere ouverte");
+            autreBiere.ouverte= true;
+        } else {
+            System.out.println("erreur : biere déjà ouverte");
+        }
+        System.out.println(autreBiere) ;
+        BouteilleBiere deuxBiere = new BouteilleBiere("Heineken", 2.0 ,"Marseille") ;
+        deuxBiere.ouverte = false;
+         if (deuxBiere.Decapsuler()) {
+            System.out.println("biere ouverte");
+            deuxBiere.ouverte= true;
+        } else {
+            System.out.println("erreur : biere déjà ouverte");
+        }
+        System.out.println(deuxBiere) ;
+        BouteilleBiere troisBiere = new BouteilleBiere("Desperados", 12.0 ,"Fronti") ;
+        troisBiere.ouverte = true;
+         if (troisBiere.Decapsuler()) {
+            System.out.println("biere ouverte");
+            troisBiere.ouverte= true;
+        } else {
+            System.out.println("erreur : biere déjà ouverte");
+        }
+        System.out.println(troisBiere) ;
+        BouteilleBiere quatreBiere = new BouteilleBiere("Coca", 4.0 ,"Paris") ;
+        quatreBiere.ouverte = false;
+         if (quatreBiere.Decapsuler()) {
+            System.out.println("biere ouverte");
+            quatreBiere.ouverte= true;
+        } else {
+            System.out.println("erreur : biere déjà ouverte");
+        }
+       System.out.println(quatreBiere) ;
+
+
 
     }
     
