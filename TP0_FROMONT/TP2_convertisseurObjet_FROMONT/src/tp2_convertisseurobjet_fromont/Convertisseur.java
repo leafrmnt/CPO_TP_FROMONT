965c9bac42417148af0f3b3ada26f552a1955a0b
lea <lea.fromont@epfedu.fr>
2023-10-13 16:15:13 +0200

    TP2 exo2 fini

--- TP0_FROMONT/TP2_convertisseurObjet_FROMONT/src/tp2_convertisseurobjet_fromont/Convertisseur.java
+++ TP0_FROMONT/TP2_convertisseurObjet_FROMONT/src/tp2_convertisseurobjet_fromont/Convertisseur.java
@@ -1,25 +1,37 @@
+package tp2_convertisseurobjet_fromont;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tp2_convertisseurobjet_fromont;
 
 /**
  *
  * @author leafr
  */
-class Convertisseur {
-
-    void CelsiusVersFahrenheit(double tempCelsius) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+public class Convertisseur {
+    private int nbConversions;
+    public Convertisseur() {
+        nbConversions = 0; //initialisation nb de conversions
+    }
+    public void CelsiusVersFahrenheit(double celsius) { //conversion celsius vers fahrenheit
+        double fahrenheit = (celsius*9/5) + 32; //calcul de la température en fahrenheit
+        nbConversions++; //+1 au compteur de nb de conversions
+        System.out.println(celsius + " °C = " + fahrenheit + " °F"); //affichage résulat
+    }
+    public void CelsiusVersKelvin(double celsius) {
+        double kelvin = celsius+273.15;
+        nbConversions++;
+        System.out.println(celsius + " °C = " + kelvin + " K");
+    }
+    public void KelvinVersFahrenheit(double kelvin) {
+        double fahrenheit = (kelvin-273.15)*9/5+32;
+        nbConversions++;
+        System.out.println(kelvin + " K = " + fahrenheit + " °F");
     }
 
-    void CelsiusVersKelvin(double tempCelsius) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    @Override
+    public String toString() {
+        return "Nb de conversions " + nbConversions;
     }
-
-    void KelvinVersFahrenheit(double tempKelvin) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
 }
