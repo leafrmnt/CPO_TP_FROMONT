d61def2f215a5ce3bb2a93213d209ffe1a7fd72a
lea <lea.fromont@epfedu.fr>
2023-10-27 11:22:00 +0200

    LightOff version améliorée cours

--- TP0_FROMONT/LightOff_FROMONT_version_console/src/lightoff_fromont_version_console/Partie.java
+++ TP0_FROMONT/LightOff_FROMONT_version_console/src/lightoff_fromont_version_console/Partie.java
@@ -13,41 +13,53 @@ import java.util.Scanner;
 public class Partie {
     GrilleDeJeu grille;
     int nbCoups;
+    
     public Partie() {
         grille = new GrilleDeJeu(7, 7);
         nbCoups =0;
     }
-public void initialiserPartie() {
-    grille.melangerMatriceAleatoirement(5); 
-}
-    /**
-     *
-     */
+    public void initialiserPartie() {
+        grille.eteindreToutesLesCellules();
+        grille.melangerMatriceAleatoirement(1); // Mélanger la grille en premier
+         
+    }
+   
     public void lancerPartie() {
-        Scanner choix = new Scanner(System.in);
-        System.out.println(grille.toString());
-        String choixJoueur;
-        nbCoups = 0;
-        while (!grille.cellulesToutesEteintes()) {
-            System.out.println("\n Entrer le coup que vous voulez jouer : /n DD pour la diagonale descendante (DD) /n 1C pour la premiere colonne /n 2C pour la deuxieme colonne /n 3C /n 4C /n 5C /n 6C /n 7C /n 1L pour la premiere ligne /n 2L /n 3L /n 4L /n 5L /n 6L /n 7L /n DM");
-            choixJoueur = choix.next();
-            if (choixJoueur.equals("L")) {
-                grille.activerLigneDeCellules(nbCoups);
-            } else if (choixJoueur.equals("C")) {
-                grille.activerColonneDeCellules(nbCoups);
-            } else if (choixJoueur.equals("DD")) {
-                grille.activerDiagonaleDescendante();
-            } else if (choixJoueur.equals("DM")) {
-                grille.activerDiagonaleMontante();
-            } else {
-                System.out.println(".Veuillez saisir un des numero indique");
-                continue; 
-            }
+    Scanner choix = new Scanner(System.in);
+    System.out.println(grille.toString());
+    int coupsRestants = 10; // Nombre de coups maximum, ajustez-le selon votre choix
+    nbCoups = 0;
+
+    while (!grille.cellulesToutesEteintes() && coupsRestants > 0) {
+        System.out.println("\nIl vous reste " + coupsRestants + " coups. Entrez le coup que vous voulez jouer (par exemple,\n1 pour activer la première ligne,\n2 pour activer la deuxième ligne,\n-1 pour activer la première colonne,\n-2 pour activer la deuxième colonne,\n8 pour activer la diagonale descendante,\n9 pour activer la diagonale montante) :");
+
+        int choixJoueur = choix.nextInt();
+
+        if (choixJoueur > 0 && choixJoueur <= grille.nbLignes) {
+            grille.activerLigneDeCellules(choixJoueur - 1);
+        } else if (choixJoueur < 0 && choixJoueur >= -grille.nbColonnes) {
+            grille.activerColonneDeCellules(-choixJoueur - 1);
+        } else if (choixJoueur == 7) {
+            grille.activerDiagonaleDescendante();
+        } else if (choixJoueur == 8) {
+            grille.activerDiagonaleMontante();
+            continue; // Coup invalide, ne pas incrémenter nbCoups ni décrémenter coupsRestants
+        } else {
+            System.out.println("Coup invalide. Veuillez réessayer.");
+            continue; // Coup invalide, ne pas incrémenter nbCoups ni décrémenter coupsRestants
+        }
+
         nbCoups++;
+        coupsRestants--;
+
         System.out.println("Grille après le coup " + nbCoups + " :");
         System.out.println(grille.toString());
-        }
-        
+    }
+
+    if (grille.cellulesToutesEteintes()) {
+        System.out.println("Bravo ! Vous avez gagné en " + nbCoups + " coups.");
+    } else {
+        System.out.println("Perdu ! Vous avez déjà joué tous vos coups.");
     }
 }
- 
+}
